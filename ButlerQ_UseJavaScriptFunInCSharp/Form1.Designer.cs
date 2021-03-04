85542d50309cf88eea991b680b90049431b2a4c3
Quinn Butler <qeb@usf.edu>
2021-03-04 18:55:43 -0500

    Initial creation of Windows Form

--- ButlerQ_UseJavaScriptFunInCSharp/Form1.Designer.cs
+++ ButlerQ_UseJavaScriptFunInCSharp/Form1.Designer.cs
@@ -28,13 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.SuspendLayout();
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PrintDialog printDialog1;
     }
 }
 
