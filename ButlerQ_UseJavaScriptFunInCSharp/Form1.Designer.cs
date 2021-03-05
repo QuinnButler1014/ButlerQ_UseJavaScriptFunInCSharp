0b2d6c4dd5e856844bc15db6efff64ff87f0946f
Quinn Butler <qeb@usf.edu>
2021-03-04 19:18:26 -0500

    Insert Form1.cs and html document

--- ButlerQ_UseJavaScriptFunInCSharp/Form1.Designer.cs
+++ ButlerQ_UseJavaScriptFunInCSharp/Form1.Designer.cs
@@ -29,19 +29,32 @@
         private void InitializeComponent()
         {
             this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
             this.SuspendLayout();
             // 
             // printDialog1
             // 
             this.printDialog1.UseEXDialog = true;
             // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(800, 450);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webBrowser1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -49,6 +62,7 @@
         #endregion
 
         private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
     }
 }
 
--- ButlerQ_UseJavaScriptFunInCSharp/Form1.cs
+++ ButlerQ_UseJavaScriptFunInCSharp/Form1.cs
@@ -7,14 +7,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
 
 namespace ButlerQ_UseJavaScriptFunInCSharp
 {
+    [ComVisible(true)]
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+            this.webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+            //Used for calling C# code in JS
+            webBrowser1.ObjectForScripting = this;
+            webBrowser1.ScriptErrorsSuppressed = false;
+            //Usef  for disabling rightclick on web browser control
+            webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            webBrowser1.AllowWebBrowserDrop = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Get current directory of app
+            string CurrentDirectory = Directory.GetCurrentDirectory();
+            //Call HTML page using navigate methodto fire web browser document completed event
+            webBrowser1.Navigate(Path.Combine(CurrentDirectory, "HTMLPageForJavaScript.html"));
+        }
+
+        private void Report()
+        {
+            //Get HTML page div from id of Div
+            HtmlElement div = webBrowser1.Document.GetElementById("reportContent");
+            //Create simple html content
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<table>");
+            sb.Append("<tr><td><B> Hi this is my report demo</B></td></tr>");
+            sb.Append("</table>");
+            //Assign content to HTML page div to display browser control
+            div.InnerHtml = sb.ToString();
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            //Focus cursor when loaded
+            webBrowser1.Focus();
+            //Call report method
+            Report();
+        }
+        public void PrintReport()
+        {
+            //Show pring dialog and call print method
+            DialogResult dr = printDialog1.ShowDialog();
+            if (dr.ToString() == "OK")
+            {
+                webBrowser1.Print();
+            }
+            else
+            {
+                return;
+            }
         }
     }
 }
